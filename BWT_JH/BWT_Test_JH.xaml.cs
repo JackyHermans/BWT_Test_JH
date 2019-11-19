edb1e16da81c556a263cf4dfeb6c5be58c6a5ab6
JackyHermans <hermans_stevens@skynet.be>
2019-11-19 12:39:21 +0100

    1st commit

---
+++ BWT_JH/BWT_Test_JH.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace BWT_JH
+{
+    /// <summary>
+    /// Interaction logic for BWT_Test_JH.xaml
+    /// </summary>
+    public partial class BWT_Test_JH : Window
+    {
+        public BWT_Test_JH()
+        {
+            InitializeComponent();          
+        }
+
+        private void WaardeOpslaan(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                string directorynaam = @"C:\BWT_Test_JH";
+
+                if (!Directory.Exists(directorynaam))
+                {
+                    Directory.CreateDirectory(directorynaam);
+                }
+                else
+                {
+                    string bestandsnaam = "Waardes.txt";
+                    string bestand = directorynaam + @"\" + bestandsnaam;
+                    string wegTeSchrijven = GekozenWaarde.ToString();
+                    
+                    if (!File.Exists(bestand))
+                    {
+                        File.WriteAllText(bestand, wegTeSchrijven);
+                    }
+                    else
+                    {
+                        File.AppendAllText(bestand, wegTeSchrijven);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("opslaan mislukt: " + ex.Message);
+            }
+        }
+
+    }
+}
---
+++ BWT_JH/Slider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace BWT_JH
+{
+    public class Slider
+    {
+        public int Input { get; set; }
+
+        public string Waarde { get; set; }
+
+        public Slider()
+        {
+            Waarde = Input.ToString();
+        }
+    }
+}
